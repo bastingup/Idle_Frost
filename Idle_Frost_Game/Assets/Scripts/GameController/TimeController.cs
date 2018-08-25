866b8090a934782761dc39dacd6bc9b1b1a69666
bastingup <Cattoaster@outlook.de>
2018-08-25 14:11:48 +0200

    First draft TimeController

---
+++ Idle_Frost_Game/Assets/Scripts/GameController/TimeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+
+    private System.DateTime lastTime;
+    private float deltaSeconds;
+
+	void Start () {
+        deltaSeconds = (float)(lastTime - System.DateTime.Now).TotalSeconds;
+	}
+	
+	
+	void Update () {
+		
+	}
+}
