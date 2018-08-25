61dfdbeb698f1ad4be874f9a5d1c6d1661a167b1
bastingup <Cattoaster@outlook.de>
2018-08-25 12:53:15 +0200

    Groundplane, Android Set Up, UI Set Up, RenderOrder, .sln

---
+++ Idle_Frost_Game/Assets/Scripts/General/RenderOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderOrder : MonoBehaviour {
+
+	private SpriteRenderer render;
+
+	void Start () {
+		render = GetComponent<SpriteRenderer>();
+        render.sortingOrder = -(int)(this.transform.position.y * 100);
+    }
+
+	void LateUpdate()
+    {
+        render.sortingOrder = -(int)(this.transform.position.y * 100);
+    }
+}
