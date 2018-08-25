80b1b63406bd9e2dfe94fc6d930a7655a61d3239
bastingup <Cattoaster@outlook.de>
2018-08-25 13:28:56 +0200

    UI bars implemented

---
+++ Idle_Frost_Game/Assets/Scripts/GameController/EcoStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcoStats : MonoBehaviour {
+
+    public int co2Value = 50;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
