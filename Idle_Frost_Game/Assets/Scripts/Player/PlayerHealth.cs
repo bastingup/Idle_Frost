0f5d1f132ffb98636fc841562fa08cda9e593f5a
bastingup <Cattoaster@outlook.de>
2018-08-30 17:19:03 +0200

    more

---
+++ Idle_Frost_Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int pleayerHealth = 75;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Idle_Frost_Game/Assets/Scripts/UI/UIBarHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBarHealth : MonoBehaviour {
+
+    private int value;
+
+    void Start () {
+        value = GameObject.Find("Player").GetComponent<PlayerHealth>().pleayerHealth;
+        this.transform.localPosition = new Vector2(-100 + value, this.transform.localPosition.y);
+    }
+	
+	
+	void Update () {
+        value = GameObject.Find("Player").GetComponent<PlayerHealth>().pleayerHealth;
+        this.transform.localPosition = new Vector2(-100 + value, this.transform.localPosition.y);
+    }
+}
