6f006c6dfaa127fc811baeb6c3cfba440d2bcfc9
bastingup <Cattoaster@outlook.de>
2019-06-22 12:00:55 +0200

    Added Unity Ad System
    Also added test organization, as well as test gameID

---
+++ Idle_Frost_Game/Assets/Scripts/GameController/AdManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class AdManager : MonoBehaviour
+{
+    public string placementId = "rewardedVideo";
+    string gameId = "3195839";
+    // TODO disable test mode before deploy
+    bool testMode = true;
+
+    private void Start()
+    {
+        Monetization.Initialize(gameId, testMode);
+        gameId = CheckForOSAndReturnGameID();
+    }
+
+    string CheckForOSAndReturnGameID()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            return "3195839";
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            return "3195838";
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void ShowAd()
+    {
+        StartCoroutine(WaitForAd());
+    }
+
+    IEnumerator WaitForAd()
+    {
+
+
+        ShowAdPlacementContent ad = null;
+        ad = Monetization.GetPlacementContent(placementId) as ShowAdPlacementContent;
+        if (ad != null)
+        {
+            ad.Show(AdFinished);
+            yield return null;
+        }
+        yield return null;
+    }
+
+    void AdFinished(ShowResult result)
+    {
+        if (result == ShowResult.Finished)
+        {
+            // TODO Implement actual reward system
+            GameObject.FindObjectOfType<EcoStats>().co2Value -= 10f;
+        }
+    }
+}
+
